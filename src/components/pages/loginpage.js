4b646e346771944c90b5c6615a22fddcfb2775c1
moa <larssonmoaml@gmail.com>
2020-10-28 22:56:11 +0100

    added beginnings of a login-page

--- src/components/pages/loginpage.js
+++ src/components/pages/loginpage.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 export default class LoginPage extends Component {
     render() {
         return (
-            <div>
-                login!!!
-            </div>
+                <form className="menu">
+                    <label for="uName">Email</label>
+                    <br/>
+                    <input type="text" id="uName" name="uName"></input>
+                    <br/>
+                    <label for="password">Lösenord</label>
+                    <br/>
+                    <input type="text" id="password" name="password"></input>
+                    <br/>
+                    <input type="submit" value="Logga in"></input>
+                </form>
         );
     }
 }
